66ebf7b673ccbb3c4e711cb255b4ab832e7f2067
Ridoy-vai <mdsahariyarridoy@gmail.com>
2026-05-19 15:51:39 +0600

    my tutor eadit funtion complite

--- index.js
+++ index.js
@@ -32,24 +32,72 @@ async function run() {
         const tutorsCollection = db.collection('tutors')
         const bookingsCollection = db.collection('bookings')
 
-
+        // add server side api
         app.post('/tutors', async (req, res) => {
             const tutors = req.body
             const result = await tutorsCollection.insertOne(tutors)
             res.send(result)
         })
 
-        
-        app.get('/tutors/:id', async (req, res) => {
-            const { id } = req.params
-            const result = await tutorsCollection.findOne({ _id: new ObjectId(id) })
-            res.send(result)
-        })
-
+        // get all tutors
         app.get('/tutors', async (req, res) => {
             const result = await tutorsCollection.find().toArray()
             res.send(result)
         })
+
+        app.patch('/tutors/:id', async (req, res) => {
+            const { id } = req.params
+            const updatedData = req.body
+            const result = await tutorsCollection.updateOne({ _id: new ObjectId(id) }, { $set: updatedData })
+            res.send(result)
+        })
+        app.get('/tutors/:userId', async (req, res) => {
+            const { userId } = req.params
+            console.log(userId)
+            const result = await tutorsCollection.find({ userId }).toArray()
+            res.send(result)
+        })
+
+        //my tutor details page api user id and tutor id both are required to get the tutor data
+        app.get('/tutors/:userId/:_id', async (req, res) => {
+            const { userId, _id } = req.params
+            console.log(userId, _id)
+            const result = await tutorsCollection.find({ userId, _id: new ObjectId(_id) }).toArray()
+            res.send(result)
+        })
+
+        //my tutor update api user id and tutor id both are required to update the tutor data
+        app.patch('/tutors/:userId/:_id', async (req, res) => {
+            const { userId, _id } = req.params
+            const updatedData = req.body
+            const result = await tutorsCollection.updateOne({ userId, _id: new ObjectId(_id) }, { $set: updatedData })
+            res.send(result)
+        })
+
+        //-------------
+
+        // get single tutor details page
+        app.get('/tutors/:_id', async (req, res) => {
+            const { _id } = req.params
+            const result = await tutorsCollection.findOne({ _id: new ObjectId(_id) })
+            res.send(result)
+        })
+
+        // add user booking data to database
+        app.post('/bookings', async (req, res) => {
+            const bookings = req.body
+            const result = await bookingsCollection.insertOne(bookings)
+            res.send(result)
+        })
+
+        // get all bookings of a user id based
+        app.get('/bookings/:userId', async (req, res) => {
+            const { userId } = req.params
+            const result = await bookingsCollection.find({ userId }).toArray()
+            res.send(result)
+        })
+
+
 
 
 
